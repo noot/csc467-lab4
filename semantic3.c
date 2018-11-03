5085b086514bcb3f727f06faff3daf0326c2b3be
farwakhan <farwakhan015@gmail.com>
2018-11-03 15:23:54 -0400

    ..

--- semantic3.c
+++ semantic3.c
@@ -2,6 +2,27 @@
 #include "semantic.h"
 #include "ast.h"
 
+void ast_check_semantics() {
+	if (DEBUG_SEMANTIC) {
+		printf("Semantic check starting\n");
+		int i = 0;
+		for (i = 0; i < 20; i++) {
+			printf("-");
+		}
+		printf("\n");
+	}
+
+	if (ast == NULL) {
+		errorOccurred = 1;
+		fprintf(errorFile, "Main scope not found.\n");
+		return;
+	}
+	else {
+		ast_traverse(ast, 0, &ast_scope_generator, &ast_sementic_check);
+
+	}
+
+}
 void ast_semantic_check(node* current) {
 
 	if (current == NULL) {
@@ -15,5 +36,12 @@ void ast_semantic_check(node* current) {
 		case(SCOPE_NODE): scope_exit(); break;
 		case(DECLARATIONS_NODE): break;
 		case(STATEMENTS_NODE): break;
+	}
+}
+
+void ast_scope_generator(node *cur, int x) { //Done pre-post.
+
+	if (cur->kind == SCOPE_NODE) {
+		scope_enter();
 	}
 }
