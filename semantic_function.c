fdeafbd29d506bf9fb08d7b030f141e8291b1552
Khan  Farwa <khanfa68@ug224.eecg.toronto.edu>
2018-11-06 18:44:29 -0500

    function now working

--- semantic_function.c
+++ semantic_function.c
@@ -2,19 +2,6 @@
 #include "semantic.h"
 #include "ast.h"
 
-#define DEBUG_FUNCTION 1
-#define DEBUG_DECLARATION 0
-#define DEBUG_CONSTRUCTOR 0
-#define DEBUG_BINARY 0
-#define DEBUG_VAR_NODE 0
-#define	DEBUG_ARGUMENTS 0
-#define DEBUG_UNARY 0
-#define DEBUG_ASSIGNMENT 0
-
-bool assign_predefined(node* curr);
-bool check_function(node* current);
-bool predefined_variables(node* current);
-
 void ast_check_semantics() {
 	if (ast == NULL) {
 		errorOccurred = 1;
@@ -33,10 +20,6 @@ void ast_scope_generator(node *cur, int x) { //Done pre-post.
 		scope_enter();
 	}
 }
-
-
-
-
 //checking function and function arguments
 
 void ast_sementic_check(node* current, int x) {
@@ -50,59 +33,78 @@ void ast_sementic_check(node* current, int x) {
 
 	switch (kind) {
 		case UNKNOWN: {break;}
-		case SCOPE_NODE: {scope_exit(); break;}
-		case DECLARATIONS_NODE: {break;} //break
-		case STATEMENTS_NODE: {break;} //break
-		case UNARY_EXPRESION_NODE: {break;} //TODO
-		case BINARY_EXPRESSION_NODE: {break;} //TOFINISH
+		case SCOPE_NODE: {scope_exit();	fprintf(errorFile,"Reached scope\n");break;}
+		case DECLARATIONS_NODE: {fprintf(errorFile,"Reached declarations node\n");break; } //break
+		case STATEMENTS_NODE: {fprintf(errorFile,"Reached statements node\n");break;} //break
+		case UNARY_EXPRESION_NODE: {
+		fprintf(errorFile,"Reached unary");		
+		break;} 
+		case BINARY_EXPRESSION_NODE: {
+			fprintf(errorFile,"Reached binary node\n");
+			int left = current->binary_expr.left->type.type_code;
+			int right = current->binary_expr.right->type.type_code;
+			int op = current->binary_expr.op;
+
+			fprintf(errorFile,"Type codes of binary expression Val left:%d Val right:%d\n",left,right);
+
+			if (op == EQ ){
+				if (left == right){
+					current->type.type_code = BOOL_T;
+					current->type.vec = 1;
+					fprintf(errorFile,"Assigning type code of boolean: %d\n",current->type.type_code);
+					break;//this wasn't there before and was causing problems!!! remember to break!!!
+				}	
+				else{
+					fprintf(errorFile,"Error: Expecting same type on both sides\n");
+					//current->type.type_code = -1;
+					break;
+				}
+			}
+		}
+			
 		case INT_NODE:{
+			fprintf(errorFile,"Reached int node\n");
 			current->type.type_code = INT_T;
 			current->type.vec = 1;
 			current->type.is_const = 1;
 			break;}
 		case FLOAT_NODE:{
+			fprintf(errorFile,"Reached float node\n");
 			current->type.type_code = FLOAT_T;
 			current->type.vec = 1;
 			current->type.is_const = 1;
 			break; }
-		case IDENT_NODE: {break;} //not needed I think
-		case VAR_NODE:{break;}
-		case FUNCTION_NODE:{ 
+		case VAR_NODE:{fprintf(errorFile,"Reached var\n");}
+		case FUNCTION_NODE:{
 			//pointer to the arguments of the function
 			node* next_arg = current->func.args;
 
 			//if no arguments, return error
-			if (next_arg == NULL){
+			if (current->func.args == NULL){
 				fprintf(errorFile, "Error: must have arguments, cannot be void\n");
 				break;
 			}
 
 			//get next arguments
-			//next_arg = next_arg->args.args;
+			next_arg = next_arg->args.args;
 	
-			if (current->func.name == 2) {
-				if (next_arg->type.type_code != FLOAT_T) {
-					fprintf(errorFile,"Error: Expecting float for first argument\n");
+			//current->type.type_code will be the return type of the function
+			if (current->func.name == 2) { //rsq
+				fprintf(errorFile,"Type of args:%d \n",next_arg->type.type_code);
+				if (!(next_arg->type.type_code == FLOAT_T ||next_arg->type.type_code == INT_T )) {
+					fprintf(errorFile,"Error: Expecting float or integer for first argument\n");
 					break;
 				}
-				else if (next_arg == NULL){
-					fprintf(errorFile, "Expecting input for first argument of function - it can't be void\n");
-					break;
-				}
+				
 				next_arg = next_arg->args.args;
-	
-				if (next_arg->type.type_code != INT_T){
-					fprintf(errorFile, "Expecting integer for second argument\n");
-					break;
+				if (next_arg != NULL){
+					fprintf(errorFile,"Error: Too many arguments for this function\n");					
 				}
-				else if (next_arg == NULL){
-					fprintf(errorFile, "Error: Expecting input for second argument of function - it can't be void\n");
-					break;
-				}
+
 			}
 
 	
-			else if (current->func.name == 0) {
+			else if (current->func.name == 0) { //dp3
 
 				//case:vec4
 				if (next_arg == NULL){
@@ -183,7 +185,7 @@ void ast_sementic_check(node* current, int x) {
 				}
 			}
 
-			else if (current->func.name == 1) {
+			else if (current->func.name == 1) { //lit
 				if (!(next_arg->type.type_code == VEC_T && next_arg->type.vec==4)) {
 					fprintf(errorFile,"Error: LIT function only takes in arguments of type vec4\n");
 					break;
@@ -195,23 +197,73 @@ void ast_sementic_check(node* current, int x) {
 			}
 		}
 
-		case CONSTRUCTOR_NODE: {break;} 
-		case ARGUMENTS_NODE:{break;}
-		case TYPE_NODE: {break;} //TODO?
+		case CONSTRUCTOR_NODE: {fprintf(errorFile,"Reached construct\n");break;} 
+		case ARGUMENTS_NODE:{	
+			fprintf(errorFile,"Reached arguments node\n");		
+			if (current->args.expr){
+				//arguments must be of expected type  
+				current->type.is_const = current->args.expr->type.is_const;	
+				fprintf(errorFile,"Type code args: %d\n",current->args.expr->type.type_code);
+				current->type.type_code = current->args.expr->type.type_code;
+				current->type.vec = current->args.expr->type.vec;
+				break;
+			}
+			else{
+				current->type.is_const = 0; //verify
+				current->type.type_code = -1; //verify
+				current->type.vec = 1;
+				break;
+				
+			}
+		}
 		case BOOL_NODE:{
+			fprintf(errorFile,"Reached bool node\n");
 			current->type.type_code = BOOL_T;
 			current->type.vec = 1;
 			current->type.is_const = 1;
 			break;}
-		case IF_STATEMENT_NODE:{break;}
-		case ASSIGNMENT_NODE:{break;}
-		case NESTED_SCOPE_NODE: {break;} //TODO
-		case NESTED_EXPRESSION_NODE: {break;} //TODO
-		case EXP_VAR_NODE: {//TODO 
+		case IF_STATEMENT_NODE:{
+			fprintf(errorFile,"Reached if condition\n");		
+			fprintf(errorFile,"Type code of conditional expression: %d\n",current->if_stmt.condition_expr->type.type_code);
+			if (!(current->if_stmt.condition_expr->type.type_code == BOOL_T && current->if_stmt.condition_expr->type.vec == 1)){ //can't be bvec
+				fprintf(errorFile, "Error: conditional expression must be of type boolean\n");	
+				break;		
+			}
+		}
+		case ASSIGNMENT_NODE:{fprintf(errorFile,"Reached assignment\n");break;}
+		case NESTED_SCOPE_NODE: {fprintf(errorFile,"Reached nested_scope\n");break;} //TODO
+		case NESTED_EXPRESSION_NODE: {
+			fprintf(errorFile,"Reached nested_exp node\n");
+			current->type.is_const = current->nested_expr.expr->type.is_const;
+		 	current->type.type_code = current->nested_expr.expr->type.type_code;
+			fprintf(errorFile,"Type code nested: %d\n",current->type.type_code);
+			current->type.vec = current->nested_expr.expr->type.vec;
+			
+		} 
+		case EXP_VAR_NODE: {
+			fprintf(errorFile,"Reached exp_var node\n");
+			current->type.is_const = current->exp_var_node.var_node->type.is_const;
+		  	current->type.type_code = current->exp_var_node.var_node->type.type_code;
+			fprintf(errorFile,"Type code exp_var: %d\n",current->type.type_code);
+			current->type.vec = current->exp_var_node.var_node->type.vec; 
 			break; }
-		case DECLARATION_NODE:{break;}
+		case DECLARATION_NODE:{		
+			fprintf(errorFile,"Reached declaration\n");		
+			symbol_table_entry new_entry;
+
+			new_entry.id = current->declaration.id;
+			new_entry.is_const = current->declaration.is_const;
+			new_entry.type_code = current->declaration.type_node->type.type_code;
+			new_entry.vec = current->declaration.type_node->type.vec;
+
+			//if initialized
+			new_entry.is_init = 1;
+
+			symbol_add(new_entry);
+		 	break;}
 		default: {break;}
 	}
 	
 }
   
+
