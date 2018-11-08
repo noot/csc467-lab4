5fae540168543983587ef5655824d1b2175b02c6
Khan  Farwa <khanfa68@ug225.eecg.toronto.edu>
2018-11-08 16:07:11 -0500

    aa

--- symbol.c
+++ symbol.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "symbol.h"
 #include "common.h"
 
 entry* table = NULL;
+entry* stack = NULL;
 
-void new_scope(entry *e) {
-	table->next_scope = e;
+void enter_scope(entry *e) {
+	e->this_scope = table;
+	//table->next_scope = e;
+	e->prev_scope = stack;
 	table = e;
+	stack->next_scope = table;
+}
+
+void exit_scope() {
+	stack = stack->prev_scope;
+	table = stack->this_scope;
 }
 
 void new_entry(entry *e) {
+	if(in_scope(e->id) == 1) {
+		break;
+	}
 	if(table == NULL) {
 		table = (entry *)malloc(sizeof(entry));
 	} else {
@@ -19,3 +32,13 @@ void new_entry(entry *e) {
 	table = e;
 }
 
+int in_scope(char *id) {
+	entry *curr = table;
+	while(curr) {
+		if(strcmp(curr->id, id) == 0) {
+			return 1;
+		}
+		curr = curr->this_scope;
+	}
+	return 0;
+}
--- symbol.h
+++ symbol.h
@@ -8,10 +8,13 @@ typedef struct _entry {
 	int is_vec;
 	int type_name;
 	char *id;
-	entry *next_scope;
+	entry *prev_scope;
 	entry *this_scope;
+	entry *next_scope;
 };
 
-void new_scope();
-void new_entry();
+void enter_scope();
+void exit_scope();
+void new_entry(entry *e);
+int in_scope(char *id);
 
